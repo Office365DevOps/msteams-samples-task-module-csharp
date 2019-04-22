c7f294d770edc014c43ea377e2b720250a9435ba
paul cheung <paul_cheung@msn.cn>
2019-04-22 22:14:18 +0800

    init project

---
+++ Microsoft.Teams.Samples.TaskModule.Web/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Microsoft.Teams.Samples.TaskModule.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("configure")]
+        public ActionResult Configure()
+        {
+            return View();
+        }
+
+        [Route("CreateItem")]
+        public ActionResult CreateItem()
+        {
+            ViewBag.Result = "Create Item";
+            if (Request.HttpMethod.ToUpperInvariant() == "POST")
+            {
+                var item = new ItemInfo()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = Request.Form["name"],
+                    Description = Request.Form["description"],
+                    Link = Request.Form["link"],
+                    Image = string.IsNullOrEmpty(Request.Form["image"]) ? "http://lorempixel.com/800/800?rand=" + DateTime.Now.Ticks.ToString() : Request.Form["image"],
+                    CreatedUser = Request.Form["createdUser"],
+                    CreatedTime = DateTime.Now,
+                };
+                ItemDB.AddItem(item);
+            }
+
+            return View(ViewBag);
+        }
+
+        [Route("UpdateItem/{id}")]
+        public ActionResult UpdateItem(Guid id)
+        {
+            ViewBag.Result = "Update Item";
+            var item = ItemDB.Items.Find(b => b.Id == id);
+            if (Request.HttpMethod.ToUpperInvariant() == "POST")
+            {
+                item.Name = Request.Form["name"];
+                item.Description = Request.Form["description"];
+                item.Link = Request.Form["link"];
+                item.Image = string.IsNullOrEmpty(Request.Form["image"]) ? "http://lorempixel.com/800/800?rand=" + DateTime.Now.Ticks.ToString() : Request.Form["image"];
+                item.UpdatedUser = Request.Form["updatedUser"];
+                item.UpdatedTime = DateTime.Now;
+            }
+
+            return View(item);
+        }
+
+        [Route("ListItem")]
+        public ActionResult ListItem()
+        {
+            return View();
+        }
+    }
+}
---
+++ Microsoft.Teams.Samples.TaskModule.Web/ItemDB.cs
@@ -0,0 +1,96 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Microsoft.Teams.Samples.TaskModule.Web
+{
+    public static class ItemDB
+    {
+        public static List<ItemInfo> Items;
+        static ItemDB()
+        {
+            Items = new List<ItemInfo>();
+            Items.AddRange(LoadItems());
+        }
+
+        public static void AddItem(ItemInfo item)
+        {
+            if (item == null)
+                return;
+
+            Items.Add(item);
+        }
+
+        public static List<ItemInfo> SearchItems(string nameKeyword)
+        {
+            return (from item in Items
+                    where item.Name.Contains(nameKeyword) || item.Description.Contains(nameKeyword)
+                    select item).ToList();
+        }
+
+        public static List<ItemInfo> LoadItems()
+        {
+            var random = new Random();
+            var items = new List<ItemInfo>();
+            items.Add(new ItemInfo() { Id = Guid.NewGuid(),
+                Name = "Microsoft surface Book / Book 2 1798 AC Charger",
+                Description = "Original 102W Power Adapter For Microsoft surface Book / Book 2 1798 AC Charger",
+                CreatedUser = "Griffin Liu",
+                Image = "https://c1.neweggimages.com/ProductImageCompressAll300/V017_1_201903041648712141.jpg",
+                CreatedTime = DateTime.Now,
+                Link = "https://www.newegg.com/Product/Product.aspx?Item=9SIAH9B8ZS0459&Description=book&cm_re=book-_-9SIAH9B8ZS0459-_-Product",
+                StarCount = random.Next(1000),
+                UpdatedTime = DateTime.Now.AddHours(-81536) });
+            items.Add(new ItemInfo()
+            {
+                Name = "Laptop Battery",
+                Description = "BattPit: Laptop Battery Replacement for Samsung Notebook 9 NP900X5L-K02US, AA-PBUN4AR, Notebook 9 900X5L, ATIV Book 9 Spin 940X3L (7.7V 4000mAh 31Wh)",
+                CreatedUser = "Jerry Wu",
+                Image = "https://c1.neweggimages.com/NeweggImage/ProductImageCompressAll300/A3E4_1_201805311072186346.jpg",
+                Link = "https://www.newegg.com/Product/Product.aspx?Item=9SIA3E47F30202&Description=book&cm_re=book-_-9SIA3E47F30202-_-Product",
+                StarCount = random.Next(1000),
+                CreatedTime = DateTime.Now.AddHours(-546),
+            });
+            items.Add(new ItemInfo()
+            {
+                Name = "Microsoft Surface Book 2",
+                Description = "Microsoft Surface Book 2 HNN-00001 Intel Core i7 8th Gen 8650U (1.90 GHz) 16 GB Memory 1 TB PCIe SSD NVIDIA GeForce GTX 1050 13.5",
+                CreatedUser = "Tom Yang",
+                Image = "https://images10.newegg.com/ProductImageCompressAll300/34-735-554-V01.jpg",
+                Link = "https://www.newegg.com/Product/Product.aspx?Item=9SIA3E47F30202&Description=book&cm_re=book-_-9SIA3E47F30202-_-Product",
+                StarCount = random.Next(1000),
+                CreatedTime = DateTime.Now.AddHours(-48611),
+            });
+
+            return items;
+        }
+    }
+
+    public class ItemInfo
+    {
+        [JsonProperty("Id")]
+        public Guid Id { get; set; }
+        [JsonProperty("Name")]
+        public string Name { get; set; }
+        [JsonProperty("Description")]
+        public string Description { get; set; }
+        [JsonProperty("Link")]
+        public string Link { get; set; }
+        [JsonProperty("CreatedUser")]
+        public string CreatedUser { get; set; }
+        public string UpdatedUser { get; set; }
+        [JsonProperty("Image")]
+        public string Image { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public DateTime UpdatedTime { get; set; }
+
+        [JsonProperty("WantToSee")]
+        public bool WantToSee { get; set; }
+        public int WantToSeeCount { get; set; }
+        [JsonProperty("Star")]
+        public bool Star { get; set; }
+        public int StarCount { get; set; }
+    }
+}
